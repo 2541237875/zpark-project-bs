5b03c0f91a09686b9d0e42d62cd730e8ce44a4a3
2541237875 <97875559+2541237875@users.noreply.github.com>
2023-05-06 16:43:56 +0800

    Update App.java

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,5 +5,6 @@ public class App {
         System.out.println("Hello Git");
         System.out.println("Hello Git,Second!!");
         System.out.println("Hello Git,hhhhhhh!!");
+        System.out.println("Test pull from Github!");
     }
 }
